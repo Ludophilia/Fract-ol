132d8a18cbc2809aa99754d917f8d668a011c3f0
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-07-06 22:34:32 +0200

    Added a very rough color set

--- imgmgr.c
+++ imgmgr.c
@@ -6,18 +6,28 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 17:54:38 by jgermany          #+#    #+#             */
-/*   Updated: 2023/07/06 21:11:21 by jgermany         ###   ########.fr       */
+/*   Updated: 2023/07/06 22:34:04 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "imgmgr.h"
 #include <stdio.h>
 
-#define WHITE	0x00FFFFFF
-#define BLACK	0x00000000
-#define RED		0x00FF0000
-#define GREEN	0x0000FF00
-#define BLUE	0x000000FF
+#define WHITE		0x00FFFFFF
+#define BLACK		0x00000000
+#define RED			0x00FF0000
+#define GREEN		0x0000FF00
+#define BLUE		0x000000FF
+
+#define MAX_VALUE	50.0
+#define MAX_ITER	100
+
+// Need a struct or something to store the window and plane info?
+
+// -1, -1.75, -0.4 + 0.6 * I, -0.2 + 0.7 * I, I
+// -0.835 - 0.2321 * I, -0.608 + 0.510 * I, -0.25 + 0.646 * I
+// 0.3; Can't see it because no black zones. Only color
+// gradients will give me something here.
 
 int	init_image(t_mlx *mlx_data)
 {
@@ -59,73 +69,25 @@ static void	colorize_pixel(int x, int y, t_img *img_con, uint32_t color)
 	}
 }
 
-// ############################################################################
-
-// What to do?
-// Find a plotting algorithm for the Julia/mandelbrot
-
-// The logic roughly goes like this
-
-// JULIA
-
-// Starting point z(0) = x + yi, matching the point (x, y) on screen.
-// Sequence: z = z²+ c, with c being an arbitrary complex
-
-// MANDELBROT
-
-// Starting point z(0) = 0
-// Sequence: z = z²+ c, with c being x + yi, matching the point (x, y) on screen.
-
-// The point should be coloured black or white (for now) according to
-// if the sequence diverge to infinity or not
-
-// Let's start with the mandelbrot.
-
-// ############################################################################
-
-// Oh, shit, there is like many pitfalls I copmpletely overlooked...
-
-// The mandelbrot set appears between [-2, 1] on Re(z) and [-1.5, 1.5] on Im(z)
-// The julia set, between [-1.5, 1.5] on Re(z) and [-1, 1] on Im(z)
-// So our complex plane could be [-1.5, 1.5] on Re(z) and [-1.5, 1.5] on Img(z) to start
-
-// Plus, MLX's 0,0 is on the top left when our plan (0, 0) is on the center...
-// So we have to find a way to convert the matrix 640x480 (4:3) to somethig close to
-// [-2, 2] Re(z) and [1.5, 1.5] Img(z) with 0 in the center.
-
-// So the magic should happen here... It's here that... The x and y coordinates
-// on the 640 * 480 WINDOW should be PASSED to test_mandelbrot() and then converted
-// to their equivalent on a small 4 * 3 complex plane that starts at -2, -1.5.
-
-// That way... :
-// (0,0) 
-
-// (320, 240) should be used to represent (0,0) 
-
-// (640)
-
 int	test_mandelbrot(double x, double y, int max_iter)
 {
 	double complex 	z;
 	int				i;
 
 	i = -1;
-	// Need a struct or something to store the window and plane info?
-	x = -2.0 + ((2.0 - -2.0)  / WINDOW_X) * x;
+	x = -2.0 + ((2.0 - -2.0) / WINDOW_X) * x;
 	y = 1.0 - ((1.0 - -1.0) / WINDOW_Y) * y;
 	z = 0;
-	while (++i < max_iter) // creal(z) <= 10 && cimg(z) <= 10
+	while (++i < max_iter)
 	{
-		// creal(z) > 100.0 || cimag(z) > 100.0)
-		// if (sqrt((creal(z) * creal(z)) + (cimag(z) * cimag(z))) > 50.0)
-		if (sqrt(z * conj(z)) > 50.0)
-			return (1); // Goes to infinity
+		if (sqrt(z * conj(z)) > MAX_VALUE)
+			return (i);
 		z = (z * z);
 		z += (x + y * I);
 	}
-	// printf("zmax(%i, %i) = %.lf + %.lfi\n", x, y, creal(z), cimag(z));
-	return (0); // stable
+	return (max_iter);
 }
+
 
 int	test_julia(double x, double y, int max_iter)
 {
@@ -133,28 +95,47 @@ int	test_julia(double x, double y, int max_iter)
 	int				i;
 
 	i = -1;
-	// Need a struct or something to store the window and plane info?
 	x = -2 + ((2.0 - -2.0)  / WINDOW_X) * x;
 	y = 1.0 - ((1.0 - -1.0) / WINDOW_Y) * y;
-	// printf("x = %lf ; y = %lfi\n", x, y);
 	z = (x + y * I);
-	// printf("z = %lf + %lfi\n", creal(z), cimag(z));
-	while (++i < max_iter) // creal(z) <= 10 && cimg(z) <= 10
+	while (++i < max_iter)
 	{
-		// if (csqrt(cpow(creal(z), 2) + cpow(cimag(z), 2)) > (double complex)1000.0)
-		// if (sqrt((creal(z) * creal(z)) + (cimag(z) * cimag(z))) > 50.0)
-		// if (creal(z) > 50.0 && cimag(z) > 50.0)
-		if (sqrt(z * conj(z)) > 50.0)
-			return (1); // Goes to infinity
-		z = (z * z); // Valid values for c:
-		z += -0.25 + 0.646 * I; // 0.3; Can't see it because no black zones. Only color gradients will give me something here.
-		// -1, -1.75, -0.4 + 0.6 * I, -0.2 + 0.7 * I, I
-		// -0.835 - 0.2321 * I, -0.608 + 0.510 * I, -0.25 + 0.646 * I
+		if (sqrt(z * conj(z)) > MAX_VALUE)
+			return (i);
+		z = (z * z);
+		z += 0.3;//-0.4 + 0.6 * I;
 	}
-	// printf("zmax(%lf, %lf) = %lf + %lfi\n", x, y, creal(z), cimag(z));
-	return (0); // stable
+	return (max_iter);
 }
 
+// The idea now is to start from base colors and then use the iter_max to
+// generate new colors close to the original...
+int	colorize_fractal(int iter_max)
+{
+	if (iter_max == MAX_ITER)
+		return BLACK;
+	else
+		return 0x00011C40 + 8 * iter_max;
+
+	// else if ((iter_max / 10) == 1)
+	// 	return 0x00022859;
+	// else if ((iter_max / 10) == 2)
+	// 	return 0x00033E8C;
+	// else if ((iter_max / 10) == 3)
+	// 	return 0x00034AA6;
+
+
+	// else if ((iter_max / 10) == 4)
+		// return 0x00FFCC0D; // Yellow
+	// else if ((iter_max / 10) == 5)
+	// 	return 0x00FF7326;
+	// else if ((iter_max / 10) == )
+	// 	return 0xBF2669;
+	// else if ((iter_max / 10) == 9)
+	// 	return 0xFF194D;
+	// else
+	// 	return WHITE;
+}
 
 int	draw_on_scene(t_mlx *mlx_data, t_img *img_con)
 {
@@ -168,10 +149,7 @@ int	draw_on_scene(t_mlx *mlx_data, t_img *img_con)
 		x = -1;
 		while (++x < WINDOW_X)
 		{
-			if (test_julia(x, y, 100) == 1)
-				color = WHITE;
-			else
-				color = BLACK;
+			color = colorize_fractal(test_mandelbrot(x, y, MAX_ITER));
 			colorize_pixel(x, y, img_con, color);
 		}
 	}

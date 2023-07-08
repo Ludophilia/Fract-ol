a9e262b18207ca46766ff04f4c01cd627e3bc5ba
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-07-09 00:05:03 +0200

    Tried the histogram method for coloring, but it seems to complex for the time remaining...

--- experiments/color/imgmgr.c
+++ experiments/color/imgmgr.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 17:54:38 by jgermany          #+#    #+#             */
-/*   Updated: 2023/07/09 00:02:50 by jgermany         ###   ########.fr       */
+/*   Updated: 2023/07/08 23:33:24 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #define BLUE		0x000000FF
 
 #define MAX_VALUE	50.0
-#define MAX_ITER	150
+#define MAX_ITER	100
 
 // Need a struct or something to store the window and plane info?
 
@@ -64,6 +64,20 @@ static void	colorize_pixel(int x, int y, t_img *img_con, uint32_t color)
 	}
 }
 
+// The idea now is to start from base colors and then use the iter_max to
+// generate new colors close to the original...
+int	colorize_fractal0(int iter_max)
+{
+	int color;
+
+	color = 255 - (int)(iter_max * 255 / MAX_ITER); // Pourcent of a hue
+	// 255 - (100 * 255/100) // -> 0 (black) when iter_max == 100 == MAX_ITER.
+	// 255 - (255/100) -> 255 - 2 // -> 253 (black) when iter_max == 1.
+	// 255 - (0.75 * 255) -> 255 - 2 // -> 253 (black) when iter_max == 75.
+	return (color << 16 | color << 8 | color); // GREYSCALES SAIKOU
+}
+
+
 int	test_mandelbrot(double x, double y, int max_iter)
 {
 	double complex 	z;
@@ -98,7 +112,7 @@ int	test_julia(double x, double y, int max_iter)
 		if ((creal(z) * creal(z) + cimag(z) * cimag(z)) > 4)
 			return (i);
 		z = (z * z);
-		z += -0.4 + 0.6 * I;
+		z += -0.2 + 0.7 * I;
 	}
 	return (max_iter);
 }
@@ -106,72 +120,92 @@ int	test_julia(double x, double y, int max_iter)
 // -0.835 - 0.2321 * I, -0.608 + 0.510 * I, -0.25 + 0.646 * I
 // 0.3; 0.285
 
-int	colorize_fractal(int iter_max)
-{
-	int color;
-
-	// THIS. Give me a palette, maybe try linear interpolation on it. 
-	// NO HISTOGRAMS. The higher the indice, the more time the tested sequence 
-	// takes to escape and the warmer the color...
-	// int	palette[] = {1, 2, 3, 4, 5};
-
-	color = 255 - (int)(iter_max * 255 / MAX_ITER); // Pourcent of a hue
-	// 255 - (255/100) -> 255 - 2 // -> 253 (black) when iter_max == 1.
-	// 255 - (0.75 * 255) -> 255 - 2 // -> 253 (black) when iter_max == 75.
-	// 255 - (100 * 255/100) // -> 0 (black) when iter_max == 100 == MAX_ITER.
-	return (color << 16 | color << 8 | color);
-}
 
 // int	colorize_fractal(int iter_max)
 // {
+// 	int	iterations_ppx[WINDOW_Y][WINDOW_X];
 
-
-// 	if (iter_max == MAX_ITER)
-// 		return BLACK;
-// 	else if (iter_max >= 0 && iter_max < 50)
-// 		return 0x011C40 + 7 * iter_max; // GOOD
-// 	// else if (iter_max >= 33 && iter_max < 66)
-// 	// 	return 0x011C40 + (10 * (iter_max - 30)); // 0x8c0709
-// 	// else if (iter_max >= 66 && iter_max < 99)
-// 	// 	return 0x011C40 + 10 * (iter_max - 60);
-// 	else
-// 		return WHITE;
-// 	// else if ((iter_max / 10) == 1)
-// 	// 	return 0x00022859;
-// 	// else if ((iter_max / 10) == 2)
-// 	// 	return 0x00033E8C;
-// 	// else if ((iter_max / 10) == 3)
-// 	// 	return 0x00034AA6;
-
-
-// 	// else if ((iter_max / 10) == 4)
-// 		// return 0x00FFCC0D; // Yellow
-// 	// else if ((iter_max / 10) == 5)
-// 	// 	return 0x00FF7326;
-// 	// else if ((iter_max / 10) == )
-// 	// 	return 0xBF2669;
-// 	// else if ((iter_max / 10) == 9)
-// 	// 	return 0xFF194D;
-// 	// else
-// 	// 	return WHITE;
+// 	// The first pass involves calculating the iteration counts 
+// 	// associated with each pixel (but without any pixels being plotted).
 // }
 
 int	draw_on_scene(t_mlx *mlx_data, t_img *img_con)
 {
 	int	x;
 	int	y;
-	int	color;
 
+	int	iterations_ppx[WINDOW_Y][WINDOW_X]; // IterationCounts
+	int	iteration_freq[MAX_ITER]; // NumIterationsPerPixel
+
+	int iter_max;
+
+	ft_bzero(iteration_freq, MAX_ITER * sizeof(int));
 	y = -1;
 	while (++y < WINDOW_Y)
 	{
 		x = -1;
 		while (++x < WINDOW_X)
 		{
-			color = colorize_fractal(test_julia(x, y, MAX_ITER));
-			colorize_pixel(x, y, img_con, color);
+			iter_max = test_mandelbrot(x, y, MAX_ITER);
+			iterations_ppx[y][x] = iter_max; // first pass
+			iteration_freq[iter_max] += 1; // second pass
+			// colorize_fractal(iter_max);
+			// colorize_pixel(x, y, img_con, color);
 		}
 	}
+
+//	######################################################################
+
+	// float	hue[WINDOW_Y][WINDOW_X];
+	int total;
+	// int	i;
+	
+	iter_max = -1;
+	total = 0;
+	while (++iter_max < MAX_ITER)
+		total += iteration_freq[iter_max]; // third pass
+
+	(void)iterations_ppx;
+	// y = -1;
+	// while (++y < WINDOW_Y) // fourth pass
+	// {
+	// 	x = -1;
+	// 	while (++x < WINDOW_X)
+	// 	{
+	// 		iter_max = iterations_ppx[y][x];
+	// 		i = -1;
+	// 		hue[y][x] = 0;
+	// 		while (++i < iter_max)
+	// 			hue[y][x] += iteration_freq[i] / total;
+	// 	}
+	// }
+
+// ######################################################################
+
+	(void)colorize_pixel;
+
+	// int	palette[5] = {
+	// 	0xFFFFFF,
+	// 	0xFFFFFF,
+	// 	0xFFFFFF,
+	// 	0xFFFFFF,
+	// 	0x0,
+	// }; // From low to high iteration. 
+	// int	color;
+
+	// y = -1;
+	// while (++y < WINDOW_Y)
+	// {
+	// 	x = -1;
+	// 	while (++x < WINDOW_X)
+	// 	{
+	// 		color = palette[ (int)(hue[y][x] * 5) * 100 ];
+	// 		colorize_pixel(x, y, img_con, color);
+	// 	}
+	// }
+
+//	######################################################################
+
 	mlx_put_image_to_window(mlx_data->mlx_ptr, mlx_data->win_ptr,
 		img_con->img_ptr, 0, 0);
 	return (0);
